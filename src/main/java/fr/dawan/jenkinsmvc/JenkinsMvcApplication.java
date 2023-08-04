d51fa72cdf0b608ef422073b3ec78e0ac672a55d
MaxSurm <surmontmaxime@gmail.com>
2023-08-04 11:07:34 +0200

    Initial commit

---
+++ src/main/java/fr/dawan/jenkinsmvc/JenkinsMvcApplication.java
@@ -0,0 +1,13 @@
+package fr.dawan.jenkinsmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsMvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JenkinsMvcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fr/dawan/jenkinsmvc/JenkinsMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.dawan.jenkinsmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsMvcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
